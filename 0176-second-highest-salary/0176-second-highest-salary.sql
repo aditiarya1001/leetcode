595b5a8e26b0ff4a4e72adb022d788d60f16f3eb
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-07-19 09:46:13 +0530

    Time: 406 ms (91.50%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -4,16 +4,16 @@
 # from employee where salary < (select max(salary) from employee );
 
 #  by limit
-SELECT
-    IFNULL(
-      (SELECT DISTINCT Salary
-       FROM Employee
-       ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1),
-    NULL) AS SecondHighestSalary
+# SELECT
+#     IFNULL(
+#       (SELECT DISTINCT Salary
+#        FROM Employee
+#        ORDER BY Salary DESC
+#         LIMIT 1 OFFSET 1),
+#     NULL) AS SecondHighestSalary
 
 # by correalted subquery
 
-# select salary as SecondHighestSalary from employee e1 where 2 =
-# (select count(distinct e2.salary) from 
-# employee e2 where e2.salary>=e1.salary);
+select ifnull(max(salary),null) as secondhighestsalary
+from employee
+where salary < some(select max(salary) from employee)
