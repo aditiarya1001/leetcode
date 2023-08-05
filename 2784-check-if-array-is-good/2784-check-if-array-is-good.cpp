62db3922bdf9b487aea4918ce6e6be4d13ba95e6
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-05 22:28:27 +0530

    Time: 16 ms (24.08%), Space: 23.9 MB (9.65%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
+        // int n = nums.size()-1;
+        // vector<int>temp;
+        // for(int i=1; i<n; i++) 
+        //     temp.push_back(i);
+        // temp.push_back(n);
+        // temp.push_back(n);
+        // sort(nums.begin(), nums.end());
+        // if(nums == temp)
+        // {
+        //     return true;
+        // }
+        // return false;
         int n = nums.size()-1;
-        vector<int>temp;
-        for(int i=1; i<n; i++) 
-            temp.push_back(i);
-        temp.push_back(n);
-        temp.push_back(n);
-        sort(nums.begin(), nums.end());
-        if(nums == temp)
-        {
-            return true;
-        }
-        return false;
+        map<int,int>mp;
+        for(int i=1; i<n; i++) mp[i]=1;
+        mp[n]=2;
+        for(auto &i : nums) 
+            mp[i]--;
+        for(auto &i : mp) 
+            if(i.second) 
+                return false;
+        
+        return true;
     }
 };
