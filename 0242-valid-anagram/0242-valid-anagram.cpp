b4979d2c05fd1f61badbc37147f3daa8bbfeace3
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-05 11:04:05 +0530

    Time: 6 ms (83.52%), Space: 7.4 MB (11.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+    //     if(s.length()!=t.length())
+    //     {
+    //         return false;
+    //     }
+    //     unordered_map<char,int> s1,t1;
+    //     for(int i=0;i<s.length();i++)
+    //     {
+    //         s1[s[i]]++;
+    //         t1[t[i]]++;
+    //     }
+    //     if(s1!=t1)  //it compares each and every key value pairs
+    //     {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+        
         if(s.length()!=t.length())
         {
             return false;
         }
-        unordered_map<char,int> s1,t1;
+        unordered_map<char,int> mp;
         for(int i=0;i<s.length();i++)
         {
-            s1[s[i]]++;
-            t1[t[i]]++;
+            mp[s[i]]++;
+            mp[t[i]]--;
         }
-        if(s1!=t1)
+        for(auto it:mp)
         {
-            return false;
+            if(it.second!=0) return false;
         }
+
         return true;
     }
 };
