3a2040de21c6f887d08946ac4f5876bfd30247b2
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-05 22:37:44 +0530

    Time: 11 ms (55.34%), Space: 23.9 MB (9.65%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -13,14 +13,14 @@ public:
         //     return true;
         // }
         // return false;
-        int n = nums.size()-1;
+        int n = nums.size();
         unordered_map<int,int>mp;
-        for(int i=1; i<n; i++) mp[i]=1;
-        mp[n]=2;
-        for(auto &i : nums) 
-            mp[i]--;
+        for(int i=1; i<n-1; i++) mp[i]=1;
+        mp[n-1]=2;
+        for(int i =0;i<n;i++) 
+            mp[nums[i]]--;
         for(auto &i : mp) 
-            if(i.second) 
+            if(i.second!=0) 
                 return false;
         
         return true;
