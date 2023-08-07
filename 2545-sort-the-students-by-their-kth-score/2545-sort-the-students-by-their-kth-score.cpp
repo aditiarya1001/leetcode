8989b1b61b204a0525e3b9a7639fd249958e9e35
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-07 22:18:54 +0530

    Time: 50 ms (95.36%), Space: 31 MB (19.41%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
-        map<int,int,greater<int>> temp;
-        int idx=0;
-        for(auto i:score){
-            temp[i[k]]=idx;
-            idx++;
-        }
-        vector<vector<int>> res;
-        for(auto i: temp){
-            res.push_back(score[i.second]);
-        }
-        return res;
-    
+        // map<int,int,greater<int>> temp;
+        // int idx=0;
+        // for(auto i:score){
+        //     temp[i[k]]=idx;
+        //     idx++;
+        // }
+        // vector<vector<int>> res;
+        // for(auto i: temp){
+        //     res.push_back(score[i.second]);
+        // }
+        // return res;
+        
+        
+    map<int,vector<int>,greater<int>>mp;
+    for(auto &i : score) mp[i[k]] = i;
+        
+    vector<vector<int>> res;
+    for(auto i: mp)
+    {
+        res.push_back(i.second);
+    }
+    return res;
+    // score.clear();
+    // for(auto i = mp.rbegin(); i!=mp.rend(); i++) 
+    //     score.push_back(i->second);
+    // return score;
     }
 };
