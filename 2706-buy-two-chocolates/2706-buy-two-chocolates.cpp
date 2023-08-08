04c3b0ac14a501d932a3f12693c9988e2d50befe
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-08 13:49:47 +0530

    Time: 11 ms (95.92%), Space: 46.8 MB (8.16%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,38 +1,50 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
+        
         int n = prices.size();
         sort(prices.begin(), prices.end());
-        int sum = prices[0] + prices[1];
-        if(sum > money)
+        vector<int> prefix(n, 0);
+        for (int i = 0; i < n; i++) 
         {
+            prefix[i] = (prices[i] + (i == 0 ? 0 : prefix[i - 1]));
+        }
+        if (prefix[1] > money ) 
+        {
+            if(prices[0]>money)
+            {
+                return money;
+            }
+            else 
+            {
             return money;
-        }
-        else
+            }
+            
+        } 
+        
+        else if (prefix[1] < money) 
         {
-            return (money - sum);
+            return (money-prefix[1]);
+        } 
+        else 
+        {
+            return 0;
         }
+    
+        // int n = prices.size();
+        // sort(prices.begin(), prices.end());
+        // int sum = prices[0] + prices[1];
+        // if(sum > money)
+        // {
+        //     return money;
+        // }
+        // else
+        // {
+        //     return (money - sum);
+        // }
     }
 };
 
 
 
-// int n = prices.size();
-//         sort(prices.begin(), prices.end());
-//         vector<int> prefix(n, 0);
-//         for (int i = 0; i < n; i++) 
-//         {
-//             prefix[i] = (prices[i] + (i == 0 ? 0 : prefix[i - 1]));
-//         }
-//         if (prefix[1] > money) 
-//         {
-//             return money - prefix[1];
-//         } 
-//         else if (prefix[1] < money) 
-//         {
-//             return prefix[0];
-//         } 
-//         else 
-//         {
-//             return 0;
-//         }
+// 
