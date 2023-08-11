278c8e843816bbbdd73f670381ea0d101e1d632d
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-11 14:12:35 +0530

    Time: 6 ms (25.80%), Space: 6.9 MB (5.91%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int n=s.length();
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return t[i]; //ye loop frst case k according bs d tak check krega in abcd
+            }
+        }
+        return t[n]; //koi equal nhi hoga last wla toh hoga hi kunki t mei ek extra h as per                              problem
+    }
+};
