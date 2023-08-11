aecc91ad86b1772f0ec08c2c32cb7bbcabed972d
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-11 23:44:10 +0530

    Time: 3 ms (76.59%), Space: 9.6 MB (26.75%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        int n = mat.size();   // n = 3 (number of rows)
-        int m = mat[0].size(); // m = 3 (number of columns)
-
-        int i = 0, j = m*n - 1;
-        while(i<=j){
-            int mid = (i + j)/2;
-            //to trace the indices of mid
-            int r = mid/m;
-            int c = mid%m;
+        int n= mat.size(); //number of rows
+        int m= mat[0].size();// number of columns
+        
+        int i= 0;
+        int j= m*n-1;
+        
+        while(i<=j)
+        {
+            int mid= (i+j)/2;
+            //to find indices of mid
+            int r= mid/m;
+            int c= mid%m;
+            
             if(mat[r][c]==target)
+            {
                 return true;
-            if(mat[r][c]>target)
+            }
+            else if(mat[r][c]>target)
             {
                 j = mid - 1;
             }
@@ -20,5 +26,60 @@ public:
                 i = mid + 1;
         }
         return false;
-      }
+        }
 };
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//     int n = mat.size();   // n = 3 (number of rows)
+//         int m = mat[0].size(); // m = 3 (number of columns)
+
+//         int i = 0, j = m*n - 1;
+//         while(i<=j){
+//             int mid = (i + j)/2;
+//             //to trace the indices of mid
+//             int r = mid/m;
+//             int c = mid%m;
+//             if(mat[r][c]==target)
+//                 return true;
+//             if(mat[r][c]>target)
+//             {
+//                 j = mid - 1;
+//             }
+//             else 
+//                 i = mid + 1;
+//         }
+//         return false;
+//       }
