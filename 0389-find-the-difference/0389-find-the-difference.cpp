db2a6e302f739b0b1b6b1909e2e103cebdc1b56a
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-11 14:19:35 +0530

    Time: 0 ms (100.00%), Space: 7 MB (5.91%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        int n=s.length();
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.length();i++)
+        // int n=s.length();
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // for(int i=0;i<s.length();i++)
+        // {
+        //     if(s[i]!=t[i])
+        //     {
+        //         return t[i]; //ye loop frst case k according bs d tak check krega in abcd
+        //     }
+        // }
+        // return t[n]; //koi equal nhi hoga last wla toh hoga hi kunki t mei ek extra h as per                              problem
+        
+        unordered_map<char,int>mp;
+        
+        for(const auto &c : s)
         {
-            if(s[i]!=t[i])
-            {
-                return t[i]; //ye loop frst case k according bs d tak check krega in abcd
-            }
+            mp[c]++;
         }
-        return t[n]; //koi equal nhi hoga last wla toh hoga hi kunki t mei ek extra h as per                              problem
+         for(const auto &c:t) 
+         {
+             if(--mp[c]<0) 
+             {
+                 return c;
+             }
+         }
+        return '\0';
     }
 };
