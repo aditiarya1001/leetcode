4fa5619d42a29939fd547a43aae88b017dcbb6c5
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-14 15:30:08 +0530

    Time: 5 ms (28.02%), Space: 6.7 MB (11.38%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,19 +1,51 @@
 class Solution {
 public:
-    int lengthOfLastWord(string s) {
-        int n=s.length();
-        int count=0;
-        for(int i=n-1;i>=0;i--)
+//     int lengthOfLastWord(string s) {
+//         int n=s.length();
+//         int count=0;
+//         for(int i=n-1;i>=0;i--)
+//         {
+//             if(s[i]!=' ')
+//             {
+//                 count++;
+//             }
+//             else if(count>0 && s[i]==' ')
+//             {
+//                 break;
+//             }
+//         }
+//         return count;
+//     }
+// };
+    
+         int lengthOfLastWord(string s)          
+         {
+             stack<char> s1;
+       int count=0;
+
+       for(int i=0;i<s.size();i++)
+       {
+           s1.push(s[i]);
+       }
+
+        while(s1.top()==' ')
         {
-            if(s[i]!=' ')
-            {
-                count++;
-            }
-            else if(count>0 && s[i]==' ')
-            {
-                break;
-            }
+            s1.pop();
         }
-        return count;
+        /*
+now we run the while loop util the stack is empty in the case of 
+only one word is there or the blank space comes which seperate the two words.
+
+*/
+
+      while(!s1.empty() && s1.top()!=' '){
+        count++;
+        s1.pop();    
+    }
+    
+// now we itterate the count to count the length of the last word and return  it.
+
+    return count;
+
     }
 };
