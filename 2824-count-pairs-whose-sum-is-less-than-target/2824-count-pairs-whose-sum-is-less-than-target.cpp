03ade7d324eaae0bc84818fa9196e6e752878510
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-24 11:59:26 +0530

    Time: 0 ms (100.00%), Space: 20.5 MB (61.11%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
-    int countPairs(vector<int>& nums, int target) {
-        int count=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=i+1;j<nums.size();j++)
-            {
-                if(nums[i]+nums[j]<target && i<j)
-                {
-                    count++;
-                }
-            }
-        }
-        return count;
+//     int countPairs(vector<int>& nums, int target) {
+//         //brute force
+//         int count=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             for(int j=i+1;j<nums.size();j++)
+//             {
+//                 if(nums[i]+nums[j]<target && i<j)
+//                 {
+//                     count++;
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+int countPairs(vector<int>& nums, int target) {     
+    sort( nums.begin() , nums.end()); 
+       int i =0 , j = nums.size()-1 ;
+       int cnt =0 ; 
+       while( i < j)
+       { 
+           if( nums[i]+ nums[j] < target)
+           {
+               cnt = cnt + (j-i) ; 
+               i++;
+           }
+           else
+           {
+               j--;
+           } 
+       }
+       return cnt ;
     }
 };
