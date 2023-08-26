ab2f3eb4539f934ef6129fa491bd636f610f45c4
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-08-26 12:57:14 +0530

    Time: 7 ms (90.62%), Space: 8.9 MB (43.56%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.cpp
@@ -1,18 +1,27 @@
+// class Solution {
+// public:
+//     string removeTrailingZeros(string num) {
+//         int indx = num.size() - 1; // Initialize with the last index
+        
+//         for (int i =0;i<num.size();i++) {
+//             if (num[i] != '0') {
+//                 indx = i; // Update indx to the index of the last non-zero character
+//             }
+//         }
+        
+//         string ans = "";
+//         for (int j = 0; j < indx+1; j++) {
+//             ans += num[j];
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    string removeTrailingZeros(string num) {
-        int indx = num.size() - 1; // Initialize with the last index
-        
-        for (int i =0;i<num.size();i++) {
-            if (num[i] != '0') {
-                indx = i; // Update indx to the index of the last non-zero character
-            }
+        string removeTrailingZeros(string num) {
+        int i = num.size()-1;
+        while(num[i]=='0') i--;
+        return num.substr(0, i+1);
         }
-        
-        string ans = "";
-        for (int j = 0; j < indx+1; j++) {
-            ans += num[j];
-        }
-        return ans;
-    }
 };
