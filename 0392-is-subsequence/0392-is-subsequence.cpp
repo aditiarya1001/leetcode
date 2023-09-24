c1841ec9c0f5b40c6013ea3a63d3f92ad8f11296
Aditi Arya <112799213+aditiarya1001@users.noreply.github.com>
2023-09-24 11:05:28 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (9.24%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int n = s.size();
+        int i = 0, count = 0;
+        int j = 0;
+
+        while (i < n && j < t.size()) { // Check if we are within bounds of both strings
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == n; // Check if we successfully matched all characters in s
+    }
+};
